b73ed76ab0a1ccbb1cce9a7f1eb6a3b284912ade
hayfatouzi <hayfa.touzi@esprit.tn>
2024-03-13 09:56:26 +0100

    first commit

---
+++ fonction.c
@@ -0,0 +1,13 @@
+#include"fenetre.h"
+
+void init_image2(struct_img *background){
+background->image=IMG_Load("perso.png");
+//background->positionfromimage.x=120;
+//background->positionfromimage.y=132;
+//background->positionfromimage.h=205;
+//background->positionfromimage.w=360;
+//background->positioninscrenn.x=150;
+//background->positioninscrenn.y=260;
+};
+ 
+$
